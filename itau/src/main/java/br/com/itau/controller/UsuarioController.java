032cf4be4961f975859dd7b0dbbfd8cf395d078f
Humberto Sousa <professorhumbertodelgado@gmail.com>
2021-05-18 14:20:13 -0300

    Atualizacao

--- itau/src/main/java/br/com/itau/controller/UsuarioController.java
+++ itau/src/main/java/br/com/itau/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.com.itau.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,32 +22,56 @@ public class UsuarioController {
 	private UsuarioDAO dao;
 	
 	
+	@PostMapping("/login")
+	public ResponseEntity<Usuario> logar(@RequestBody Usuario objeto){
+		List<Usuario> lista = (List<Usuario>) dao.findAll();
+		for (Usuario usuario : lista) {
+			if (usuario.getEmail().equals(objeto.getEmail()) && usuario.getSenha().equals(objeto.getSenha())) {
+				return ResponseEntity.ok(usuario);
+			}
+		}
+		return ResponseEntity.status(404).build();
+	}
+	
 	@GetMapping("/userget/{cod}")
-	public Usuario getUserGet(@PathVariable int cod) {
+	public ResponseEntity<Usuario> getUserGet(@PathVariable int cod) {
 		Usuario resposta = dao.findById(cod).orElse(new Usuario());
 		resposta.setSenha("");
-		return resposta;
+		if (resposta.getId()==0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(resposta);
 	}
 	
 	@PostMapping("/userpost")
-	public Usuario getUserPost(@RequestBody Usuario objeto) {
+	public ResponseEntity<Usuario> getUserPost(@RequestBody Usuario objeto) {
 		Usuario resposta= dao.findById(objeto.getId()).orElse(new Usuario());
 		resposta.setSenha("");
-		return resposta;
+		if (resposta.getId()==0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(resposta);
 		
 	}
 	
-	
 	@PostMapping("/novousuario")
-	public Usuario add(@RequestBody Usuario objeto) {
-		dao.save(objeto);
-		return objeto;
+	public ResponseEntity<Usuario> add(@RequestBody Usuario objeto) {
+		try {
+			dao.save(objeto);
+			return ResponseEntity.ok(objeto);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(403).build();
+		}
 	}
 	
 	@GetMapping("/users")
-	public List<Usuario> getAll(){
+	public ResponseEntity<List<Usuario>> getAll(){
 		List<Usuario> lista = (List<Usuario>) dao.findAll();
-		return lista;
+		if (lista.size()==0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(lista);
 	}
 	
 	
